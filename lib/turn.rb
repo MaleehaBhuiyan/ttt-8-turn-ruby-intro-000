6161cc26e57e705ca127ffe0700373596b052d1b
Maleeha Bhuiyan <maleeha.bhuiyan@baruchmail.cuny.edu>
2020-04-10 16:39:04 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -41,8 +41,8 @@ def turn(board)
   puts "Please enter 1-9:"
   user_input = gets.strip
   index = input_to_index(user_input)
-  if valid_move?(board,index) == true
-    move(board_array,index,player="X")
+  if valid_move?(board,player) == true
+    move(board,player="X")
   else
     turn(board)
   end
